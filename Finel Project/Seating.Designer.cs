1fdc6c20d450a25349dfa8fe7da910c9b75e79d4
yehiel <yehiel33@gmail.com>
2020-06-05 16:05:55 +0300

    הוספת פונקציות לחלון הושבה

--- Finel Project/Seating.Designer.cs
+++ Finel Project/Seating.Designer.cs
@@ -30,9 +30,19 @@
         {
             this.lblSeating = new System.Windows.Forms.Label();
             this.btnBack = new System.Windows.Forms.Button();
-            this.lblTable = new System.Windows.Forms.Label();
             this.cmbGuestList = new System.Windows.Forms.ComboBox();
             this.btnLoadGestList = new System.Windows.Forms.Button();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.rb8seats = new System.Windows.Forms.RadioButton();
+            this.rb10seats = new System.Windows.Forms.RadioButton();
+            this.rb12seats = new System.Windows.Forms.RadioButton();
+            this.gbNumberOfSeats = new System.Windows.Forms.GroupBox();
+            this.btnAddToTable = new System.Windows.Forms.Button();
+            this.listTable = new System.Windows.Forms.ListBox();
+            this.btnSaveTable = new System.Windows.Forms.Button();
+            this.btnOurRecommendation = new System.Windows.Forms.Button();
+            this.lblRecommendation = new System.Windows.Forms.Label();
+            this.gbNumberOfSeats.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblSeating
@@ -51,7 +61,7 @@
             // 
             this.btnBack.BackColor = System.Drawing.Color.MediumTurquoise;
             this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.btnBack.Location = new System.Drawing.Point(352, 359);
+            this.btnBack.Location = new System.Drawing.Point(21, 811);
             this.btnBack.Name = "btnBack";
             this.btnBack.Size = new System.Drawing.Size(129, 57);
             this.btnBack.TabIndex = 51;
@@ -59,47 +69,159 @@
             this.btnBack.UseVisualStyleBackColor = false;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
-            // lblTable
-            // 
-            this.lblTable.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblTable.Location = new System.Drawing.Point(293, 106);
-            this.lblTable.Name = "lblTable";
-            this.lblTable.Size = new System.Drawing.Size(160, 145);
-            this.lblTable.TabIndex = 52;
-            // 
             // cmbGuestList
             // 
             this.cmbGuestList.FormattingEnabled = true;
-            this.cmbGuestList.Location = new System.Drawing.Point(57, 204);
+            this.cmbGuestList.Location = new System.Drawing.Point(21, 558);
             this.cmbGuestList.Name = "cmbGuestList";
-            this.cmbGuestList.Size = new System.Drawing.Size(121, 24);
+            this.cmbGuestList.Size = new System.Drawing.Size(221, 24);
             this.cmbGuestList.TabIndex = 53;
+            this.cmbGuestList.Visible = false;
             // 
             // btnLoadGestList
             // 
             this.btnLoadGestList.BackColor = System.Drawing.Color.MediumTurquoise;
             this.btnLoadGestList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.btnLoadGestList.Location = new System.Drawing.Point(40, 262);
+            this.btnLoadGestList.Location = new System.Drawing.Point(123, 505);
             this.btnLoadGestList.Name = "btnLoadGestList";
-            this.btnLoadGestList.Size = new System.Drawing.Size(159, 57);
+            this.btnLoadGestList.Size = new System.Drawing.Size(221, 34);
             this.btnLoadGestList.TabIndex = 54;
             this.btnLoadGestList.Text = "Load Gest List";
             this.btnLoadGestList.UseVisualStyleBackColor = false;
             this.btnLoadGestList.Click += new System.EventHandler(this.btnLoadGestList_Click);
             // 
+            // lblWelcome
+            // 
+            this.lblWelcome.BackColor = System.Drawing.Color.DarkCyan;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblWelcome.Location = new System.Drawing.Point(12, 285);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(441, 50);
+            this.lblWelcome.TabIndex = 55;
+            this.lblWelcome.Text = "Please choose the size of the tables";
+            this.lblWelcome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // rb8seats
+            // 
+            this.rb8seats.AutoSize = true;
+            this.rb8seats.Location = new System.Drawing.Point(6, 21);
+            this.rb8seats.Name = "rb8seats";
+            this.rb8seats.Size = new System.Drawing.Size(75, 21);
+            this.rb8seats.TabIndex = 56;
+            this.rb8seats.TabStop = true;
+            this.rb8seats.Text = "8 seats";
+            this.rb8seats.UseVisualStyleBackColor = true;
+            // 
+            // rb10seats
+            // 
+            this.rb10seats.AutoSize = true;
+            this.rb10seats.Location = new System.Drawing.Point(175, 20);
+            this.rb10seats.Name = "rb10seats";
+            this.rb10seats.Size = new System.Drawing.Size(83, 21);
+            this.rb10seats.TabIndex = 57;
+            this.rb10seats.TabStop = true;
+            this.rb10seats.Text = "10 seats";
+            this.rb10seats.UseVisualStyleBackColor = true;
+            // 
+            // rb12seats
+            // 
+            this.rb12seats.AutoSize = true;
+            this.rb12seats.Location = new System.Drawing.Point(353, 20);
+            this.rb12seats.Name = "rb12seats";
+            this.rb12seats.Size = new System.Drawing.Size(83, 21);
+            this.rb12seats.TabIndex = 58;
+            this.rb12seats.TabStop = true;
+            this.rb12seats.Text = "12 seats";
+            this.rb12seats.UseVisualStyleBackColor = true;
+            // 
+            // gbNumberOfSeats
+            // 
+            this.gbNumberOfSeats.Controls.Add(this.rb8seats);
+            this.gbNumberOfSeats.Controls.Add(this.rb12seats);
+            this.gbNumberOfSeats.Controls.Add(this.rb10seats);
+            this.gbNumberOfSeats.Location = new System.Drawing.Point(12, 338);
+            this.gbNumberOfSeats.Name = "gbNumberOfSeats";
+            this.gbNumberOfSeats.Size = new System.Drawing.Size(441, 52);
+            this.gbNumberOfSeats.TabIndex = 59;
+            this.gbNumberOfSeats.TabStop = false;
+            // 
+            // btnAddToTable
+            // 
+            this.btnAddToTable.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnAddToTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnAddToTable.Location = new System.Drawing.Point(21, 620);
+            this.btnAddToTable.Name = "btnAddToTable";
+            this.btnAddToTable.Size = new System.Drawing.Size(221, 34);
+            this.btnAddToTable.TabIndex = 60;
+            this.btnAddToTable.Text = "Add to table";
+            this.btnAddToTable.UseVisualStyleBackColor = false;
+            this.btnAddToTable.Click += new System.EventHandler(this.btnAddToTable_Click);
+            // 
+            // listTable
+            // 
+            this.listTable.FormattingEnabled = true;
+            this.listTable.ItemHeight = 16;
+            this.listTable.Location = new System.Drawing.Point(269, 558);
+            this.listTable.Name = "listTable";
+            this.listTable.Size = new System.Drawing.Size(188, 228);
+            this.listTable.TabIndex = 61;
+            this.listTable.Visible = false;
+            // 
+            // btnSaveTable
+            // 
+            this.btnSaveTable.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnSaveTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnSaveTable.Location = new System.Drawing.Point(269, 811);
+            this.btnSaveTable.Name = "btnSaveTable";
+            this.btnSaveTable.Size = new System.Drawing.Size(188, 57);
+            this.btnSaveTable.TabIndex = 62;
+            this.btnSaveTable.Text = "Save Table";
+            this.btnSaveTable.UseVisualStyleBackColor = false;
+            // 
+            // btnOurRecommendation
+            // 
+            this.btnOurRecommendation.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnOurRecommendation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnOurRecommendation.Location = new System.Drawing.Point(12, 84);
+            this.btnOurRecommendation.Name = "btnOurRecommendation";
+            this.btnOurRecommendation.Size = new System.Drawing.Size(245, 42);
+            this.btnOurRecommendation.TabIndex = 63;
+            this.btnOurRecommendation.Text = " Our recommendation";
+            this.btnOurRecommendation.UseVisualStyleBackColor = false;
+            this.btnOurRecommendation.Click += new System.EventHandler(this.btnOurRecommendation_Click);
+            // 
+            // lblRecommendation
+            // 
+            this.lblRecommendation.BackColor = System.Drawing.SystemColors.Window;
+            this.lblRecommendation.Font = new System.Drawing.Font("French Script MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblRecommendation.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblRecommendation.Location = new System.Drawing.Point(14, 148);
+            this.lblRecommendation.Name = "lblRecommendation";
+            this.lblRecommendation.Size = new System.Drawing.Size(439, 114);
+            this.lblRecommendation.TabIndex = 64;
+            this.lblRecommendation.Visible = false;
+            // 
             // Seating
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(539, 555);
+            this.ClientSize = new System.Drawing.Size(469, 879);
+            this.Controls.Add(this.lblRecommendation);
+            this.Controls.Add(this.btnOurRecommendation);
+            this.Controls.Add(this.btnSaveTable);
+            this.Controls.Add(this.listTable);
+            this.Controls.Add(this.btnAddToTable);
+            this.Controls.Add(this.gbNumberOfSeats);
+            this.Controls.Add(this.lblWelcome);
             this.Controls.Add(this.btnLoadGestList);
             this.Controls.Add(this.cmbGuestList);
-            this.Controls.Add(this.lblTable);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.lblSeating);
             this.Name = "Seating";
             this.Text = "Seating";
             this.Load += new System.EventHandler(this.Seating_Load);
+            this.gbNumberOfSeats.ResumeLayout(false);
+            this.gbNumberOfSeats.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -107,8 +229,17 @@
         #endregion
         private System.Windows.Forms.Label lblSeating;
         private System.Windows.Forms.Button btnBack;
-        private System.Windows.Forms.Label lblTable;
         private System.Windows.Forms.ComboBox cmbGuestList;
         private System.Windows.Forms.Button btnLoadGestList;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.RadioButton rb8seats;
+        private System.Windows.Forms.RadioButton rb10seats;
+        private System.Windows.Forms.RadioButton rb12seats;
+        private System.Windows.Forms.GroupBox gbNumberOfSeats;
+        private System.Windows.Forms.Button btnAddToTable;
+        private System.Windows.Forms.ListBox listTable;
+        private System.Windows.Forms.Button btnSaveTable;
+        private System.Windows.Forms.Button btnOurRecommendation;
+        private System.Windows.Forms.Label lblRecommendation;
     }
 }
--- Finel Project/Seating.cs
+++ Finel Project/Seating.cs
@@ -36,6 +36,8 @@ namespace Finel_Project
 
         private void btnLoadGestList_Click(object sender, EventArgs e)
         {
+            cmbGuestList.Visible = true;
+            listTable.Visible = true;
             // הגדרת מחרוזת שמכילה את פקודת החיבור
             string strDb = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\Public\Finel Project\Finel Project.accdb;" + "Persist Security Info=False";
             // יצירת חיבור חדש לבסיס הנתונים
@@ -60,8 +62,91 @@ namespace Finel_Project
             {
                 conn.Close();
             }
+        }
 
-            
+        private void btnAddToTable_Click(object sender, EventArgs e)
+        {
+
+            if (rb8seats.Checked)
+            {
+                if (listTable.Items.Count < 8)
+                {
+                    listTable.Items.Add(cmbGuestList.Text);
+                    cmbGuestList.Items.Remove(cmbGuestList.Text);
+                }
+                else
+                    MessageBox.Show("the table is full");
+           
+            }
+            else if (rb10seats.Checked)
+            {
+                if (listTable.Items.Count < 10)
+                {
+                    listTable.Items.Add(cmbGuestList.Text);
+                    cmbGuestList.Items.Remove(cmbGuestList.Text);
+                }
+                else
+                    MessageBox.Show("the table is full");
+            }
+            else //(rb12seats.Checked)
+            {
+                if (listTable.Items.Count < 12)
+                {
+                    listTable.Items.Add(cmbGuestList.Text);
+                    cmbGuestList.Items.Remove(cmbGuestList.Text);
+                }
+                else
+                    MessageBox.Show("the table is full");
+            }
+        }
+
+        private void btnOurRecommendation_Click(object sender, EventArgs e)
+        {
+            // הגדרת מחרוזת שמכילה את פקודת החיבור
+            string strDb = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\Public\Finel Project\Finel Project.accdb;" + "Persist Security Info=False";
+            // יצירת חיבור חדש לבסיס הנתונים
+            OleDbConnection conn = new OleDbConnection(strDb);
+            OleDbCommand cmd = new OleDbCommand("Select count (*) From GUEST_LIST where [Event Owner]='" + EventSeatingManager.globalusername + "';", conn);
+
+            try
+            {
+                conn.Open();
+                int guestnumber = (Int32) cmd.ExecuteScalar();  // הגדרת אובייקט קריאה
+                lblRecommendation.Visible = true;
+                int modulo8 = guestnumber % 8;
+                int modulo10 = guestnumber % 10;
+                int modulo12 = guestnumber % 12;
+
+                
+                if(modulo8< modulo10 && modulo8 < modulo12)
+                {
+                    int numberoftables = (guestnumber / 8);
+                    lblRecommendation.Text = "You have " + guestnumber + " guests on your guest list.\nOur recommendation to you is to reserv " + numberoftables + " tables of 8 and one another table of 8 to " + modulo8 + " remaining guests.";
+                }
+                else if (modulo10<modulo8 && modulo10<modulo12)
+                { 
+                    int numberoftables = (guestnumber / 10);
+                    lblRecommendation.Text = "You have " + guestnumber + " guests on your guest list.\nOur recommendation to you is to reserv " + numberoftables + " tables of 10 and one another table of 8 to " + modulo10 + " remaining guests.";
+                }
+                else if (modulo12 < modulo8 && modulo12 < modulo8)
+                {
+                    int numberoftables = (guestnumber / 12);
+                    lblRecommendation.Text = "You have " + guestnumber + " guests on your guest list.\nOur recommendation to you is to reserv " + numberoftables + " tables of 12 and one another table of 8 to " + modulo12 + " remaining guests.";
+                }
+                else
+                {
+                    lblRecommendation.Text = "You have " + guestnumber + " guests on your guest list.\nwe didn't find recommendation for you.";
+                }
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally //תוספת אפשרית למבנה טריי וקאצ' מגדירה מה יקרה בכל מקרה
+            {
+                conn.Close();
+            }
         }
     }
 }
